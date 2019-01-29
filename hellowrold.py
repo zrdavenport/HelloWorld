c384110edf1315a1c02b045a4e9ebf64acfa0e23
Davenport <zdavenport@acumensolutions.com>
2019-01-28 20:40:12 -0500

    added files

--- hellowrold.py
+++ hellowrold.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def get_index():
+    return 'Index...'
+
+@app.route('/hello/<name>')
+def get_hello(name):
+    return render_template('hello.html', name=name)
+
